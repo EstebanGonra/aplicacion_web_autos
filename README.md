3578240cfdbc463b01789c5f17ef3d9c646b0955
EstebanGonra <estebanybarbara2015@gmail.com>
2024-02-18 09:48:11 -0300

    app actualizada

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # aplicacion_web_autos
-app_test_sprint_5
+Esta es una aplicación interactiva sobre autos, precios y características de los  mismos. 
+A través de gráficos interactivos, pueden elegirse distintas características de los vehículos y obtener gráficos sobre precios, kilometraje entre otros valores importantes.
+La aplicación está diseñada para ser informativa e interactiva.
---
+++ app.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import streamlit as st
+import plotly.express as px
+cars = pd.read_csv(r'\Users\esteb\aplicacion_web_autos\vehicles_us.csv')
+st.header('Análisis de vehículos y sus características')
+st.write('La presente es una aplicación interactiva en la que se puede seleccionar diferentes características y así obtener gráficos representativos')
+st.write('Los datos consignados fueron extraídos de la base de datos vehícles_us.csv')
+hist_button = st.checkbox('Construir histograma')  # crear un botón
+scatter_button = st.checkbox(
+    'Construir gráfico de dispersión')  # crear segundo botón
+
+if hist_button:  # al hacer clic en el botón
+    # escribir un mensaje
+    st.write(
+        'Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+
+    # crear un histograma
+    figh = px.histogram(cars, x="odometer")
+
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(figh, use_container_width=True)
+if scatter_button:  # al hacer clic en el botón
+    # escribir un mensaje
+    st.write('Creación de un gráfico de dispersión para el conjunto de datos de anuncios de venta de coches')
+
+    # crear un gráfico de dispersión
+    figs = px.scatter(cars, x="odometer", y="price")
+
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(figs, use_container_width=True)
